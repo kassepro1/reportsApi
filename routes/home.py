19405eb062a57998a286125d99fd360fb19a4675
kassepro1 <momamartallakasse@gmail.com>
2019-10-14 17:40:33 +0000

    Mis en place api authentification mis en place api permettant d'avoir la liste des missions

--- routes/home.py
+++ routes/home.py
@@ -1,5 +1,5 @@
-from flask import current_app, Blueprint, request, render_template, redirect, url_for, session, jsonify
-from flagshipcore.flagship.libserver import QualiticsFlagship
+from flask import current_app, Blueprint, request, render_template, redirect, url_for, session, jsonify, json
+from flagshipcore.flagship.libserver import QualiticsFlagship, QMission
 from flagshipcore.flagship.codes import QualiticsFlagshipException
 from models.forms import Form
 
@@ -50,9 +50,11 @@ def login():
         return jsonify(data)
 
 
-@home_bp.route('/logout/<string:token>', methods=["GET", "POST"])
-def logout(token):
+@home_bp.route('/logout', methods=["GET", "POST"])
+def logout():
     rest = create_qflagship()
+
+    token = request.headers.get("Authorization", None)
 
     try:
         if token is not None and rest.authenticated(token):
@@ -72,29 +74,36 @@ def logout(token):
         return jsonify(data)
 
 
-
 @home_bp.route('/index', methods=["GET", "POST"])
 def show():
     # TODO select client and mission type + svg model
     rest = create_qflagship()
-    token = request.cookies.get('token')
-    form = Form()
-    msg_manager = current_app.config['MSG_MANAGER']
-
-    # Clear session in case mission was previously loaded
-    session.clear()
+    token = request.headers.get("Authorization", None)
 
     if request.method == 'GET':
         try:
             if token is not None and rest.authenticated(token):
                 # Get missions from the Server to display them in the table view
-                form.missions = rest.adm_get_missions()
-            else:
-                return redirect(url_for('home.login'))
-        except QualiticsFlagshipException as ex:
-            msg_manager.append_error(ex)
 
-    return render_template("index.html", form=form, messages=msg_manager.messages)
+                missions = []
+
+                for m in rest.adm_get_missions():
+                    missions.append(
+                        {'id': m.id, 'name': m.name, 'type': m.type, 'startDate': m.start_date, 'length': m.length,
+                         'cam_count': m.cam_count, 'ir_count': m.ir_count, 'irrec_count': m.irrec_count,
+                         'status': m.status.value})
+                    # missions.append(json.dumps({'id':m.id,'name':m.name,'type':m.type,'startDate':m.start_date,'length':m.length,'cam_count':m.cam_count}))
+
+                    data = {
+                        'missions': missions
+                    }
+                return jsonify(data)
+
+            else:
+                pass
+                # return redirect(url_for('home.login'))
+        except QualiticsFlagshipException as ex:
+            pass
 
 
 @home_bp.route("/fg/delete/error/id/<message_id>", methods=["POST"])
@@ -111,5 +120,12 @@ def delete_error_by_id(message_id):
 
     if 0 <= index_msg < len(messages):
         messages.pop(index_msg)
-
-    return "OK"
+        data = {
+            'status': 'OK'
+        }
+        return jsonify(data)
+    else:
+        data = {
+            'status': 'NOOK'
+        }
+        return jsonify(data)
